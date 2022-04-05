42bf9be08da8bb7ae203392ba0904af5df8a0dc7
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-04-05 13:22:40 +0900

    Jest関連の設定を追加

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // next.config.jsとテスト環境用のdotenvファイルが配置されたパス
+  dir: './',
+})
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const customJestConfig = {
+  collectCoverage: false,
+
+  // tsconfig.jsonのcompilerOptions.pathsの定義に合わせる
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+  },
+
+  // テストの共通設定等のセットアップ用スクリプト
+  setupFiles: ['<rootDir>/jest.setup.js'],
+
+  // 対象とするテストコードのパスパターン
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.spec.ts',
+    '<rootDir>/src/**/__tests__/**/*.spec.tsx',
+  ],
+
+  testEnvironment: 'jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
