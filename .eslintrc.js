01d1bc7c0590c7684264f0d35864e82790c58275
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-04-21 15:11:48 +0900

    import/orderのルール定義

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,15 @@ module.exports = {
   extends: ['next', 'next/core-web-vitals', 'plugin:regexp/recommended', 'prettier'],
   rules: {
     '@next/next/no-img-element': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        pathGroups: [],
+        pathGroupsExcludedImportTypes: ['builtin', 'external'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        warnOnUnassignedImports: true,
+      },
+    ],
   },
 }
